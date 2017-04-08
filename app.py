3f414ce80002d94af1d7b86ba1c9c587c4dc3fc2
Christian Templeton <christian.templeton@gmail.com>
2017-04-08 09:48:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,43 @@ def main():
 def index():
   return render_template('index.html')
 
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter, WeekdayLocator,\
+    DayLocator, MONDAY
+from matplotlib.finance import quotes_historical_yahoo_ohlc, candlestick_ohlc
+
+
+# (Year, month, day) tuples suffice as args for quotes_historical_yahoo
+date1 = (2016, 9, 13)
+date2 = (2016, 11, 24)
+
+
+mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
+alldays = DayLocator()              # minor ticks on the days
+weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
+dayFormatter = DateFormatter('%d')      # e.g., 12
+
+quotes = quotes_historical_yahoo_ohlc('GOOGL', date1, date2)
+if len(quotes) == 0:
+    raise SystemExit
+
+fig, ax = plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+ax.xaxis.set_major_locator(mondays)
+ax.xaxis.set_minor_locator(alldays)
+ax.xaxis.set_major_formatter(weekFormatter)
+#ax.xaxis.set_minor_formatter(dayFormatter)
+
+#plot_day_summary(ax, quotes, ticksize=3)
+candlestick_ohlc(ax, quotes, width=0.6)
+
+ax.xaxis_date()
+ax.autoscale_view()
+plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+
+plt.show()
+
+
 if __name__ == '__main__':
   app.run(port=33507)
