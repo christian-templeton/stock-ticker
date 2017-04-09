f187cf2967fd88e28363c1863955f9964c465f54
Christian Templeton <christian.templeton@gmail.com>
2017-04-09 10:33:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, request, redirect
-import sys
-
-name = input('What\'s your name?')
-exclaim = '!'
-multiply = exclaim * 3
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter, WeekdayLocator,\
+    DayLocator, MONDAY
+from matplotlib.finance import quotes_historical_yahoo_ohlc, candlestick_ohlc
 
 app = Flask(__name__)
 
@@ -14,7 +13,41 @@ def main():
 
 @app.route('/index')
 def index():
-    return 'Hello there ' + name + multiply
+# (Year, month, day) tuples suffice as args for quotes_historical_yahoo
+date1 = (2016, 9, 13)
+date2 = (2016, 11, 24)
+
+
+mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
+alldays = DayLocator()              # minor ticks on the days
+weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
+dayFormatter = DateFormatter('%d')      # e.g., 12
+
+quotes = quotes_historical_yahoo_ohlc('GOOGL', date1, date2)
+if len(quotes) == 0:
+    raise SystemExit
+
+fig, ax = plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+ax.xaxis.set_major_locator(mondays)
+ax.xaxis.set_minor_locator(alldays)
+ax.xaxis.set_major_formatter(weekFormatter)
+#ax.xaxis.set_minor_formatter(dayFormatter)
+
+#plot_day_summary(ax, quotes, ticksize=3)
+candlestick_ohlc(ax, quotes, width=0.6)
+
+ax.xaxis_date()
+ax.autoscale_view()
+plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+
+    return plt.show()
+
+
+
+
+
+
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
