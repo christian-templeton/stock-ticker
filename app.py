39b79ecdf1ba84a127748334fd77f5198ccd3026
Christian Templeton <christian.templeton@gmail.com>
2017-04-09 10:47:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, request, redirect
+# import libraries
+import datetime
+import pandas as pd
+import matplotlib 
 import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter, WeekdayLocator,\
-    DayLocator, MONDAY
-from matplotlib.finance import quotes_historical_yahoo_ohlc, candlestick_ohlc
+import numpy as np
+import quandl
+
+# import api key
+quandl.ApiConfig.api_key = 'veCdQMWydzdrbYxvUqkr'
+
+
 
 app = Flask(__name__)
 
@@ -13,36 +21,51 @@ def main():
 
 @app.route('/index')
 def index():
-# (Year, month, day) tuples suffice as args for quotes_historical_yahoo
-date1 = (2016, 9, 13)
-date2 = (2016, 11, 24)
+# Retrieve data from quandl
+tableau = quandl.get('WIKI/DATA')
+
+# Create dataframe with most recent stock info
+df = pd.DataFrame(tableau).tail(365)
 
 
-mondays = WeekdayLocator(MONDAY)        # major ticks on the mondays
-alldays = DayLocator()              # minor ticks on the days
-weekFormatter = DateFormatter('%b %d')  # e.g., Jan 12
-dayFormatter = DateFormatter('%d')      # e.g., 12
 
-quotes = quotes_historical_yahoo_ohlc('GOOGL', date1, date2)
-if len(quotes) == 0:
-    raise SystemExit
 
-fig, ax = plt.subplots()
-fig.subplots_adjust(bottom=0.2)
-ax.xaxis.set_major_locator(mondays)
-ax.xaxis.set_minor_locator(alldays)
-ax.xaxis.set_major_formatter(weekFormatter)
-#ax.xaxis.set_minor_formatter(dayFormatter)
+df["dateform"] = [(str(df.index.month[i]) + '/' + str(df.index.date[i])[8:10] + '/' + str(df.index.date[i])[2:4]) for i in range(len(df['Adj. Close']))]
 
-#plot_day_summary(ax, quotes, ticksize=3)
-candlestick_ohlc(ax, quotes, width=0.6)
 
-ax.xaxis_date()
-ax.autoscale_view()
-plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+# Create a new data series date with the date in the preferred format using the strftime method
+df["Date"] = [df.index.strftime('%-m' + '/' + '%d' + '/' + '%y') for i in range(len(df['Adj. Close']))]
 
-    return plt.show()
+# Create a new data series with the percentage that takes the change in Adj. Close price in the preferred format using the pct_change and round methods
+df["Percentage"] =  (df['Adj. Close'].pct_change(periods=1, fill_method='pad', limit=None, freq=None)*100).round(decimals=1)
 
+
+
+
+
+
+
+
+# Create a tuple that returns the date and percentage for the first 100 days of 2016
+
+list(df.iterrows())[:5]
+
+
+# Create a new data series percentage that takes the change in Adj. Close price in the preferred format
+df["percentage"] = [float("{0:.1f}".format((df['Adj. Close'][i]/df['Adj. Close'][i-1]-1)*100)) for i in range(len(df['Adj. Close']))]
+
+df["perc"] = [df['Adj. Close'].shift(periods=1, freq=None, axis=0)*100 for i in range(len(df['Adj. Close']))]
+
+
+df["percentage"] = [float("{0:.1f}".format((df['Adj. Close'][i]/df['Adj. Close'][i-1]-1)*100)) for i in range(len(df['Adj. Close']))]
+
+
+# Create a tuple that returns the date and percentage for the first 100 days of 2016
+list(df.itertuples(index=False, name=None))[-100:][12:15]
+
+
+
+    return df["perc"] = df['Adj. Close'].shift(periods=2, freq=None, axis=1)
 
 
 
